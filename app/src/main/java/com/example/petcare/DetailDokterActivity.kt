0e165d169637820040f660049f81e0d5e7152215
Your Name <you@example.com>
2023-11-22 20:46:26 +0700

    detail dokter, detail akun, profile, riwayat, pesan

---
+++ app/src/main/java/com/example/petcare/DetailDokterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.petcare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailDokterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_dokter)
+    }
+}
